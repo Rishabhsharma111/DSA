3ec29fa44921ec922acbcbcde654396eb7c126eb
Rishabhsharma111 <rishabh132sharma@gmail.com>
2026-07-19 17:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (11.85%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+          unordered_map<char,int>mp;
+
+          for(char ch:ransomNote){
+            mp[ch]++;
+          }  
+          for(char ch:magazine){
+            mp[ch]--;
+          }
+
+          for(auto it:mp){
+            if(it.second>0)
+                return false;
+            
+          }
+
+        return true;
+    }
+};
