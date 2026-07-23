165d0ec67a90da25ae0ef20e7d0331faa09253b4
Rishabhsharma111 <rishabh132sharma@gmail.com>
2026-07-23 16:05:33 +0530

    Time: 13 ms (74.61%) | Memory: 44 MB (37.80%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -5,11 +5,11 @@ public:
         unordered_map<int,int>mp;
 
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+            mp[nums[i]]++;      //calculate frequency 
         }
         int maxi=0;
         for(auto it:mp){
-           if(mp.count(it.first + 1)){
+           if(mp.count(it.first + 1)){  //check if (it.first+1) exist or not 
             maxi=max(maxi,it.second+mp[it.first+1]);
            }
         }
