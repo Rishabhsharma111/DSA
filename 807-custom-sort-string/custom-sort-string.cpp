2982e2c8d387bf65d079a221965eae19fa439a11
Rishabhsharma111 <rishabh132sharma@gmail.com>
2026-09-08 19:25:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (41.11%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+
+
+        vector<int >freq(26,0);
+
+        for(char ch:s){
+            freq[ch-'a']++;
+
+        }
+        string ans;
+
+        for(char ch:order){
+
+                while(freq[ch-'a']>0){
+                    ans+=ch;
+                    freq[ch-'a']--;
+                }
+
+
+
+        }
+
+        for(int i=0;i<26;i++){
+            while(freq[i]>0){
+           
+                ans+=i+'a';
+            freq[i]--;
+            }
+        }
+    return ans;
+        
+    }
+};
